24b44d096bcb82a9dafcef46743341279fe163df
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2024-02-09 00:16:44 +0000

    HTML update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector('.site-header__hamburger');
 const mobileMenu = document.querySelector('.site-header__menu');
 const links = document.querySelectorAll('.site-header__menu li a');
 const main = document.querySelector('main');
+const donationAmountInput = document.querySelector('#donationAmount');
 
 hamburger.addEventListener('click', () => {
 	nav.classList.toggle('active');
@@ -18,3 +19,30 @@ links.forEach((link) =>
 		nav.classList.remove('active');
 	})
 );
+
+// ver o que está mal
+donationAmountInput.addEventListener('input', (event) => {
+	const newValue = event.target.value;
+	const number = parseFloat(newValue);
+	number.toLocaleString('pt-PT');
+
+	console.log(number);
+
+	!isNaN(event.target.value) ? (event.target.value = number) : (event.target.value = '');
+});
+
+
+inputNumber.addEventListener('input', function (event) {
+	// Obtém o valor atual do input
+	let value = event.target.value;
+
+	// Remove todos os caracteres não numéricos (exceto ponto decimal e sinal negativo)
+	value = value.replace(/[^\d.-]/g, '');
+
+	// Converte para número e formata com vírgulas e espaços
+	const formattedNumber = parseFloat(value).toLocaleString('pt-PT', { style: 'currency', currency: 'EUR' });
+
+	// Define o valor formatado de volta ao in
+	// Define o valor formatado de volta ao input
+	event.target.value = formattedNumber;
+});
